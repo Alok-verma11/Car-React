6fb2c913c20a67f0e06e5eccee784bb6c36dbc28
Alok-verma11 <vaalok185@gmail.com>
2026-03-22 11:57:16 -0800

    1.1

--- DriveElite-Car-Rental/src/appwriteConfig.js
+++ DriveElite-Car-Rental/src/appwriteConfig.js
@@ -1,16 +1,18 @@
 import { Client, Account, Databases, Storage, ID } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://fra.cloud.appwrite.io/v1")
-  .setProject(import.meta.env.VITE_PROJECT_ID); 
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("69bfe5a60000d905da24"); // Aapka Project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
-export const storage = new Storage(client);
+
+// Is line ko dhyan se check karein, yahi missing thi
 export { ID };
 
-// IDs for easy access 
-export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
-export const BUCKET_ID = import.meta.env.VITE_BUCKET_ID;
+// IDs for easy access
+export const DATABASE_ID = "69bfe5e0001ed1db92bf";
+export const storage = new Storage(client);
+export const BUCKET_ID = "69c00d3d0039e3b68e8d";
 export const BOOKINGS_COLLECTION_ID = "Bookings";
 export const CARS_COLLECTION_ID = "Cars";
